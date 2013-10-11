50be22662307e2fe663ba43cab854c7d2e9f7d7f
wiese <github@tschagrass.de>
2013-10-11 17:49:56 +0200

    selectors for flogr response parsing, handle empty result some code style

--- flogrThumbs.js
+++ flogrThumbs.js
@@ -16,31 +16,44 @@ function flogrThumbs(element, type, value) {
 	var queryData = {	// build all, pick the right one
 		set: { type: 'sets', 'setId': value },
 		tag: { type: 'recent', 'tags': value }
-	};
-	var collection = type + escape(value);
-	jQuery(element).show().addClass('initialized loading');
+	},
+	element = jQuery(element);
+
+	element.show().addClass('initialized loading');
+
 	jQuery.get('/extensions/flogr/', queryData[type], function(data) {
-		var pictures = data.match(/.*<div id=['"]thumbnail_container['"]>([\s\S]+?)<\/div>.*/i)[1];
-		var title = data.match(/.*<span id=['"]page_title['"]>([\s\S]+?)<\/span>.*/i)[1];
-		var count = pictures.match(/<img /ig).length;
-												jQuery(element).removeClass('loading').html(						'<div class="thumbs">' + pictures + '</div>' +
+		var html = jQuery.parseHTML(data),
+			pictures = jQuery('div#thumbnail_container', html).get(0).innerHTML,
+			title = jQuery('span#page_title', html).text();
+			count = jQuery('img', pictures).length;
+
+		if (jQuery.trim(pictures) === '') { // no usable content found
+			element.hide();
+			return false;
+		}
+
+		element.removeClass('loading').html(
+			'<div class="thumbs">' + pictures + '</div>' +
 			'<strong>' + title + '</strong>' +
-			'(' + count  + ')'							);
-												jQuery('a', element).attr('rel', 'lightbox[' + collection + ']');
-		jQuery('a', element).lightBox(lightboxOptions);
-												// find flickr hosted (teaser) images present in page body (too easy?)
+			'(' + count  + ')'
+		);
+		jQuery('a', element)
+			.attr('rel', 'lightbox[' + type + escape(value) + ']')
+			.lightBox(lightboxOptions);
+
+		// find flickr hosted (teaser) images present in page body (too easy?)
 		// issue: flickr using different domains (static.flickr.com, staticflickr.com)
 		var picFromFlickrRule = 'img[src*="flickr\\.com\\/"][src$="\\.jpg"]'
 		jQuery('#bodyContent p ' + picFromFlickrRule).each(function() {
-			var insiteThumb = jQuery(this);
-			var picId = insiteThumb.attr('src').match(/\/(\d{10})_.+?\.jpg$/i)[1];
+			var insiteThumb = jQuery(this),
+				picId = insiteThumb.attr('src').match(/\/(\d{10})_.+?\.jpg$/i)[1];
 			if (picId != parseInt(picId)) {
-				return true;	// give up on this, but go on to next item;
+				return true;	// give up on this, but go on to next item
 			}
-													// find respective thumb in just received list of thumbs
+			// find respective thumb in just received list of thumbs
 			jQuery(picFromFlickrRule + '[src*="/' + picId + '_"]', element).each(function() {
-				var link = jQuery(this).parent('a').get(0);	// get its link
-				var newLink = jQuery(link).clone(true).empty().get(0);
+				var link = jQuery(this).parent('a').get(0),	// get its link
+					newLink = jQuery(link).clone(true).empty().get(0);
 				insiteThumb.wrap(newLink);	// make thumb in body have same link
 			});
 		});
@@ -52,4 +65,3 @@ jQuery(document).ready(function() {
 		flogrThumbs(element, jQuery(element).data('flogr-type'), jQuery(element).data('flogr-value'));
 	});
 });
-
